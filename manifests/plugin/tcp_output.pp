4575fe169e6708a0484ee3808f1dc0d38b74a1a4
Dan Carley <dan.carley@gmail.com>
2014-12-03 18:34:46 +0000

    Add enable param to heka::plugin::tcp_output
    So that this plugin could be included on all machines and selectively
    disabled on machines that are also running `tcp_input` as an aggregator.

--- manifests/plugin/tcp_output.pp
+++ manifests/plugin/tcp_output.pp
@@ -10,11 +10,21 @@
 # [*port*]
 #   Port to send to.
 #
+# [*enable*]
+#   Disable this plugin. Useful for overriding with hiera on an aggregation
+#   machine that also has `heka::plugin::tcp_input`.
+#   Default: false
+#
 class heka::plugin::tcp_output (
   $host,
   $port,
+  $enable = true,
 ) {
-  heka::plugin { 'tcp_output':
-    content => template('heka/etc/heka/tcp_output.toml.erb'),
+  validate_bool($enable)
+
+  if $enable {
+    heka::plugin { 'tcp_output':
+      content => template('heka/etc/heka/tcp_output.toml.erb'),
+    }
   }
 }
--- spec/classes/plugin_tcp_output_spec.rb
+++ spec/classes/plugin_tcp_output_spec.rb
@@ -11,5 +11,25 @@ describe 'heka::plugin::tcp_output' do
       :content => /^address = "heka.example.com:9999"$/,
     })}
   end
-end
 
+  describe 'enable' do
+    context 'true (default)' do
+      let(:params) {{
+        :host => 'heka.example.com',
+        :port => 9999,
+      }}
+
+      it { should have_heka__plugin_resource_count(1) }
+    end
+
+    context 'true (default)' do
+      let(:params) {{
+        :host   => 'heka.example.com',
+        :port   => 9999,
+        :enable => false,
+      }}
+
+      it { should have_heka__plugin_resource_count(0) }
+    end
+  end
+end
